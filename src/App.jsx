4bacc126ba98c4761b7d195d0cbda38af87bc077
sooa <165119094+sooa02@users.noreply.github.com>
2024-10-05 18:25:34 +0900

    헤더 로그인 연결

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,24 @@
 import "./App.css";
 import Layout from "./components/Layout";
+import Join from "./pages/Join";
+import Login from "./pages/Login";
+import MyBook from "./pages/MyBook";
+import OnlyBook from "./pages/OnlyBook";
+import Search from "./pages/Search";
 
 function App() {
   return (
     <>
-    <Layout />
+      <Layout />
+
+      <Routes>
+        <Route path='/' element={<OnlyBook />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/join' element={<Join />} />
+        <Route path='/search' element={<Search />} />
+        <Route path='/mybook' element={<MyBook />} />
+      </Routes>
+
     </>
   );
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,14 @@
 import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import styled from "styled-components";
 
 const Header = () => {
   const [searchQuery, setSearchQuery] = useState('');
+  const nav = useNavigate();
+
+  const handleLoginClick = () => {
+    nav('/login');
+  };
 
   return (
     <HeaderContainer>
@@ -13,13 +19,17 @@ const Header = () => {
                   value={searchQuery}
                   onChange={(e) => setSearchQuery(e.target.value)}
                   placeholder="검색할 책의 제목을 입력해주세요."
-                  onKeyDown={(e) => e.key === 'Enter'}
-              />
+                  onKeyDown={(e) => {
+                    if (e.key === 'Enter') {
+                      // 검색 로직을 추가하거나 원하는 동작을 여기에 정의하세요
+                      console.log(`검색어: ${searchQuery}`);
+                    }
+                  }}              />
       </SearchContainer>
       <StyledButton>Home</StyledButton>
       <StyledButton>갈릭 봇</StyledButton>
       <StyledButton>나만의 책장</StyledButton>
-      <LoginButton>로그인</LoginButton>
+      <LoginButton onClick={handleLoginClick}>로그인</LoginButton>
     </HeaderContainer>
   );
 };
@@ -28,7 +38,7 @@ const HeaderContainer = styled.div`
   display: flex;
   justify-content: space-around;
   background: ${({theme})=>theme.colors.brown};
-  height: 50px
+  height: 50px;
 `;
 
 const SearchContainer = styled.div`
@@ -38,7 +48,9 @@ const SearchContainer = styled.div`
 const SearchInput = styled.input`
   flex: 1; /* 부모 컨테이너의 남은 공간을 차지하도록 설정 */
   padding: 5px; /* 여백 설정 */
-  border: 5px solid ${({ theme }) => theme.colors.brown}; /* 테두리 설정 */
+  border: 5px solid ${({ theme }) => theme.colors.brown};
+  border-radius: 10px;
+
 `;
 
 const StyledButton = styled.button`
@@ -52,11 +64,13 @@ const StyledButton = styled.button`
 
 const LoginButton = styled.button`
   display: flex;
+  justify-content: center;
+  align-items: center;
   background: ${({ theme }) => theme.colors.white};
   color: ${({ theme }) => theme.colors.brown};
-  border: 1px soild #61523F;
+  border: 1px solid #61523F;
   border-radius: 5px;
-  padding: 10px;
+  padding: 10px 20px;
   cursor: pointer;
   margin: 10px;
 `;
